b0cab6aac9c0d15f4f45d13755d2c907172b2653
adamhinton <adam.d.hinton@gmail.com>
2022-01-11 18:54:39 -0500

    Passing GET /API/POSTS tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,18 +2,27 @@
 const { Router } = require("express");
 const express = require("express");
 // const { route } = require("../server");
-// const Post = require("./posts-model");
+const Post = require("./posts-model");
 const router = express.Router();
 
-// router.get("/api/posts", (req, res) => {
-//   Post.find(req.query).then((posts) => {
-//     res.status(200).json(posts);
-//   });
+// // router.get("/api/posts", (req, res) => {
+// Post.find(req.query).then((posts) => {
+//   res.status(200).json(posts);
 // });
+// // });
 
+//Future Adam: don't be fooled by the /, this is /api/posts, all of them are.
 router.get("/", (req, res) => {
-  res.json("foo");
-  //   console.log("dfjasiosf");
+  Post.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
+    });
 });
 
 router.get("/:id", (req, res) => {});
