38a41699ae21616ee4800b18cbd1d10903785530
adamhinton <adam.d.hinton@gmail.com>
2022-01-11 18:48:56 -0500

    wrote skeleton verbs in posts-router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,5 +1,7 @@
 // implement your posts router here
+const { Router } = require("express");
 const express = require("express");
+const { route } = require("../server");
 const Post = require("./posts-model");
 const router = express.Router();
 
@@ -9,4 +11,14 @@ const router = express.Router();
 //   });
 // });
 
+router.get("/", (req, res) => {});
+
+router.get("/:id", (req, res) => {});
+router.post("/", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.get("/:id/messages", (req, res) => {});
 module.exports = router;
