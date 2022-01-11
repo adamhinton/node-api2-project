abf8263f223abc2b57cefeac96d387a228754afe
adamhinton <adam.d.hinton@gmail.com>
2022-01-11 18:36:30 -0500

    cleaned up server and started posts-router

--- api/server.js
+++ api/server.js
@@ -6,4 +6,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found A",
+  });
+});
+
 module.exports = server;
