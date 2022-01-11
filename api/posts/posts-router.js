ad162481d1b94b1aa610831dddfd01de996e9fb5
adamhinton <adam.d.hinton@gmail.com>
2022-01-11 18:42:23 -0500

    startign posts-router.js

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,8 +1,12 @@
 // implement your posts router here
 const express = require("express");
 const Post = require("./posts-model");
-const router = express.Router;
+const router = express.Router();
 
-router.length("/api/posts", (req, res) => {
-  Post.find(req.query);
-});
+// router.get("/api/posts", (req, res) => {
+//   Post.find(req.query).then((posts) => {
+//     res.status(200).json(posts);
+//   });
+// });
+
+module.exports = router;
